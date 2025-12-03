1532b6ddd5becedecc7a361650bfed9962cfa51e
Martin Wiu <martinwiu@proton.me>
2025-12-03 14:31:50 +0100

    feat: reduce tmux windows

--- work.session.sh
+++ work.session.sh
@@ -3,12 +3,6 @@ if initialize_session "work"; then
   new_window
   new_window
   new_window
-  new_window
-  new_window
-  new_window
-  new_window
-  new_window
-  new_window
   tmux move-window -t 0
 fi
 
